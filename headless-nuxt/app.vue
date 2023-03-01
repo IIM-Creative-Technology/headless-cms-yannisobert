ca3ec78707e1e0ac96a8b068a2e88345ead872fb
yannisobert <90550556+yannisobert@users.noreply.github.com>
2023-03-01 15:58:43 +0100

    headless-cms: Add files

--- headless-nuxt/app.vue
+++ headless-nuxt/app.vue
@@ -1,5 +1,14 @@
 <template>
+  <main>
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
+  </main>
 </template>
+
+<script lang="ts">
+import Index from "./pages";
+export default {
+  components: {Index}
+}
+</script>
--- headless-strapi/config/plugins.js
+++ headless-strapi/config/plugins.js
@@ -2,19 +2,19 @@ module.exports = ({ env }) => ({
   upload: {
     config: {
       provider: 'strapi-provider-upload-supabase',
-        providerOptions: {
-          apiUrl: env('SUPABASE_API_URL'),
-          apiKey: env('SUPABASE_API_KEY'),
-          bucket: env('SUPABASE_BUCKET'),
-          directory: env('SUPABASE_DIRECTORY'),
-          options: {}
+      providerOptions: {
+        apiUrl: env('SUPABASE_API_URL'),
+        apiKey: env('SUPABASE_API_KEY'),
+        bucket: env('SUPABASE_BUCKET'),
+        directory: env('SUPABASE_DIRECTORY'),
+        options: {}
       },
       breakpoints: {
-          xlarge: 1920,
-          large: 1000,
-          medium: 750,
-          small: 500,
-          xsmall: 64,
+        xlarge: 1920,
+        large: 1000,
+        medium: 750,
+        small: 500,
+        xsmall: 64,
       },
     },
   },
@@ -26,12 +26,6 @@ module.exports = ({ env }) => ({
         removeAttributesKey: true,
         removeDataKey: true,
       },
-    },
-  },
-  ckeditor: {
-    enabled: true,
-    config: {
-      plugin: {},
     },
   },
 })
